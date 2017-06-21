092cd2d6cfd88756f26d0b241af52420be439ebf
Chingiz-888 <chingiz888@mail.ru>
2017-06-21 20:35:42 +0300

    первый коммит

---
+++ Bezier/ViewController.swift
@@ -0,0 +1,76 @@
+//
+//  ViewController.swift
+//  Bezier
+//
+//  Created by Ramsundar Shandilya on 10/12/15.
+//  Copyright © 2015 Y Media Labs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var firstBezierView: BezierView!
+   
+    
+    let dataPoints = [252, 220, 101, 2, 101, 220, 252]
+    
+    var xAxisPoints : [Double] {
+        var points = [Double]()
+        for i in 0..<dataPoints.count {
+            let val = (Double(i)/6.0) * self.firstBezierView.frame.width.f
+            points.append(val)
+        }
+        
+        return points
+    }
+    
+    var yAxisPoints: [Double] {
+        var points = [Double]()
+        for i in dataPoints {
+            let val = (Double(i)/255) * self.firstBezierView.frame.height.f
+            points.append(val)
+        }
+        
+        return points
+    }
+    
+    var graphPoints : [CGPoint] {
+        var points = [CGPoint]()
+        for i in 0..<dataPoints.count {
+            let val = CGPoint(x: self.xAxisPoints[i], y: self.yAxisPoints[i])
+            points.append(val)
+        }
+        
+        return points
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        firstBezierView.dataSource = self
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        self.firstBezierView.layoutSubviews()
+    }
+
+    
+    @IBAction func refreshBtnTapped(_ sender: Any) {
+        self.firstBezierView.layoutSubviews()
+    }
+    
+    
+}
+
+extension ViewController: BezierViewDataSource {
+    
+    func bezierViewDataPoints(_ bezierView: BezierView) -> [CGPoint] {
+        
+        return graphPoints
+    }
+}
+
